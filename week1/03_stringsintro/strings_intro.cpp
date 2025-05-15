d60516107c1c5a4674ad80bd4935fa730dcd6607
JackEverhart <jeverhart9417@floridapoly.edu>
2025-05-15 16:19:00 +0000

    Strings intro

---
+++ week1/03_stringsintro/strings_intro.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    // Create a string
+    std::string s1;
+    s1 = "Hello!";
+
+    std::cout << s1 + "\n";
+
+    // Initializing a string with another string
+    std::string s2(s1);
+    std::cout << s2 << std::endl;
+
+    // Initialization with a certain number of characters
+    std::string s3(100, '#');
+    std::cout << s3 << std::endl;
+
+    // Adding strings
+    s1 += s2;
+    std::cout << s1 << std::endl;
+
+    // Initialization with a part of the string
+    std::string s4(s1, 4, 5); // start with index 4 and take 5 characters
+    std::cout << s4 << std::endl;
+
+    return 0;
+}
